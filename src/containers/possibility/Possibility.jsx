230ec37b82bd9e927c25fee22411fe285d1206da
Aitor Garcia <aitor.garcia97@gmail.com>
2022-01-15 16:16:43 +0100

    Navbar done Header done

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './possibiilty.css'
+
+const Possibiilty = () => {
+    return (
+        <div>
+            Possibiilty
+        </div>
+    )
+}
+
+export default Possibiilty
